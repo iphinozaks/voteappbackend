386c778bfe1d9bedd6090f27bd57023e01bfd1cc
iphinozaks <157301763+iphinozaks@users.noreply.github.com>
2024-02-18 20:33:35 +0800

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,10 +2,6 @@ import User from "../models/User.js";
 import { genPass,verifyPass,genToken } from "../generate/genPass.js";
 
 export const Login = async (request,response) => {
-    response.json({
-        "request": request.body
-    })
-    return;
     let user = await User.findOne({$or: [{email: request.body.name},{username: request.body.name}]})
     if (user) {
         if (verifyPass(user.password,request.body.password)) {
