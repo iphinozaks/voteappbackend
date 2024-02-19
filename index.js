11c785be11ef1949f01e9b9d542dd7d475f54d33
iphinozaks <157301763+iphinozaks@users.noreply.github.com>
2024-02-19 13:19:46 +0800

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const port  = process.env.PORT || 8000;
 const app = express();
 
 app.use(cors({
-    origin: ["https://voteappbackendapi.cyclic.app/"], 
+    origin: ["https://voterapp-c6459.web.app/"], 
     method: ["GET","POST"]
 }))
 
